df66a33b6c897024e929d7083988e89a5c10a4e4
Netochka324 <netochka324@gmail.com>
2023-01-29 22:36:04 +0300

    add preview app

--- portfolio/portfolio/views.py
+++ portfolio/portfolio/views.py
@@ -1,5 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def home(request):
-    return render(request, 'home.html')
+# def home(request):
+#     return render(request, 'home.html', {'hello': 'привет!', 'bye': 'пока!'})
+#
+#
+# def reverse(request):
+#     old_text = request.GET["usertext"]
+#     new_text = old_text[::-1]
+#     return render(request,
+#                   'reverse.html',
+#                   {'old_text': old_text, 'new_text': new_text, 'words': len(old_text.split())})
